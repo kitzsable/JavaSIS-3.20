3db1ba08816ed56d12eb4c9f76140390a532b697
Aleks <kitzsable@protonmail.com>
2020-04-21 21:02:28 +0700

    DeleteServer

--- unit-11/app-server/src/main/java/com/github/siberianintegrationsystems/restApp/data/JournalRepository.java
+++
@@ -1,8 +0,0 @@
-package com.github.siberianintegrationsystems.restApp.data;
-
-import com.github.siberianintegrationsystems.restApp.entity.Journal;
-import org.springframework.data.repository.CrudRepository;
-
-public interface JournalRepository
-        extends CrudRepository<Journal, String> {
-}
